e7f5a9f7229bd6ae18e8487c90872595fcf4ef7d
hyelin77 <nachoego4@gmail.com>
2026-06-16 15:32:54 +0900

    Update lip.js

--- lip.js
+++ lip.js
@@ -1,6 +1,6 @@
 function over(obj){
-            obj.src='images/banana.png';
+            obj.src='/images/banana.png';
         }
         function out(obj){
-            obj.src='images/apple.png';
+            obj.src='/images/apple.png';
         }
